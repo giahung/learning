c5d68ef9e48466f93deabb64dd2d6caf57b4ddac
giahung <giahung@users.noreply.github.com>
2017-03-03 08:01:29 +0700

    Add tour the heroes

---
+++ angular/heros/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './hero.component';
+import { HeroService } from './hero.service';
+
+import { AppComponent } from './app.component';
+
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot([{
+    path: 'heroes',
+    component: HeroesComponent
+  }])],
+  declarations: [AppComponent, HeroDetailComponent, HeroesComponent],
+  providers: [HeroService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
